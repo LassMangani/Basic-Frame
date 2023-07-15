292b83706715ced05a1128b57b746fa9ba61a489
LassMangani <139638412+LassMangani@users.noreply.github.com>
2023-07-15 17:15:33 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,24 @@
+
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+/**
+ *
+ * @author LMK
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // declare variables
+        MyFirstFrame myFrame;
+        
+        // instatiate the frame
+        myFrame  = new MyFirstFrame();
+        // then Run the program... ... 
+    }
+    
+}
